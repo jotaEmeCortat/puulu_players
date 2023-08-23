a62f4b9714ea56ea8940e25b9ae4452554e3efea
Jonathan Brisavoine <john.brv@gmail.com>
2023-08-23 16:08:50 +0200

    Changes before HEROKU

--- db/schema.rb
+++ db/schema.rb
@@ -10,8 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-
-ActiveRecord::Schema[7.0].define(version: 2023_08_23_122230) do
+ActiveRecord::Schema[7.0].define(version: 2023_08_23_133232) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -51,6 +50,7 @@ ActiveRecord::Schema[7.0].define(version: 2023_08_23_122230) do
     t.integer "player_price"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "status"
     t.index ["player_id"], name: "index_bookings_on_player_id"
     t.index ["user_id"], name: "index_bookings_on_user_id"
   end
